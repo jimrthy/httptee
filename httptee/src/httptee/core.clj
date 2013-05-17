b42e89b36ab1e9711ea953d4ff644694601f15e7
James Ashley <james@teamdaves.us>
2013-05-16 22:26:01 -0500

    Pushing toward what I really need. Interrupted by Real Life

--- httptee/src/httptee/core.clj
+++ httptee/src/httptee/core.clj
@@ -3,18 +3,41 @@
   (:require [org.httpkit.client :as http])
   (:gen-class))
 
+(defn -parse-headers [header-string]
+  (throw (Throwable. "Not Implemented")))
+
+(defn -parse [req]
+  "Converts the REQUEST map into the sort of map that makes sense to me"
+  (let [method-map {:get http/get} ;; other correlations seem obvious...prove through experiment
+        header-map (-parse-headers (req :headers))]
+    (let [result {:method (method-map (req :request-method))
+                  :user-agent (:user-agent header-map)
+                  :uri (req :uri)}])))
+
 (defn app [req]
   "Proxy the request to multiple backends.
 Useful for comparing results and slowly moving new functionality into production"
   ;; Most of these honestly should be parsed from request
-  (println req) 
-  (let* [options {:timeout 60000 ; ms
+  (println req)
+  
+  ;; OK, req is a map.
+  ;; Potentially Interesting keys:
+  ;; :request-method (initially :get)
+  ;; :query-string
+  ;; :uri
+  ;; :server-name
+  ;; :headers (esp. the host and user-agent
+  ;; :server-port
+  ;; :body
+  (let* [request (-parse req)
+         options {:timeout 60000 ; ms
                   :basic-auth ["user" "pass"]
-                  :user-agent "User-Agent-string"
+                  :user-agent (:user-agent request)
                   }
-         resp (http/get "http://main")
-         resp-secondary (http/get "http://secondary")
+         resp ((request :method) (request :uri) options)
+         resp-secondary ((request :method) (request :uri) options)
          response @resp]
+        (println request)
         (println "Main Response: " response)
         (println "Secondary Response: " @resp-secondary)
         response))
